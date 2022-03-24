89d1621f60886ef7f6252d80aca67738ad76c03a
Samwich422 <samanthatrevino@csus.edu>
2022-03-24 15:32:02 -0700

    update

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,4 +1,30 @@
 function alertFunc()
 {
     alert("Hello, world!");
+    document.getElementById("textbox").style.fontSize = "4em";
+}
+
+function fancyText()
+{
+    if(document.getElementById("Fancy").unchecked)
+    {
+        alert("Entering Fancy Mode");
+        var text = document.getElementById("textbox");
+        text.style = "text-decoration: underline";
+        document.getElementById("textbox").style.fontWeight = "bold";
+        document.getElementById("textbox").style.color = "blue";
+        document.getElementById("Fancy") = checked;
+
+    } 
+    else{
+        document.getElementById("Fancy") = unchecked;
+    }  
+
+}
+
+function boringText()
+{
+    document.getElementById("textbox").style.fontSize = "1.0em";
+    document.getElementById("textbox").style.color = "black";
+    document.getElementById("textbox").style.fontWeight = "normal";
 }
