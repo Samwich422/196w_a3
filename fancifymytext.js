87353068b4042e65e22992d6cc46eab9981a8ee1
Samwich422 <samanthatrevino@csus.edu>
2022-03-24 17:25:19 -0700

    Done

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,30 +1,35 @@
-function alertFunc()
-{
-    alert("Hello, world!");
+function alertFunc() {
+    alert("Hello, bigger world!");
     document.getElementById("textbox").style.fontSize = "4em";
 }
 
-function fancyText()
+function fancyText() 
 {
-    if(document.getElementById("Fancy").unchecked)
+    var text = document.getElementById("textbox");
+
+    if (document.getElementById("Fancy").checked) 
     {
         alert("Entering Fancy Mode");
         var text = document.getElementById("textbox");
         text.style = "text-decoration: underline";
-        document.getElementById("textbox").style.fontWeight = "bold";
-        document.getElementById("textbox").style.color = "blue";
-        document.getElementById("Fancy") = checked;
-
-    } 
-    else{
-        document.getElementById("Fancy") = unchecked;
-    }  
-
+        text.style.fontWeight = "bold";
+        text.style.color = "blue";
+    }
+    else if(document.getElementById("Boring").checked)
+    {
+        alert("Entering boring mode");
+        var text = document.getElementById("textbox");
+        text.style.fontSize = "1.0em";
+        text.style.color = "black";
+        text.style.fontWeight = "normal";
+        text.style.textDecoration = "none";
+    }
 }
 
-function boringText()
-{
-    document.getElementById("textbox").style.fontSize = "1.0em";
-    document.getElementById("textbox").style.color = "black";
-    document.getElementById("textbox").style.fontWeight = "normal";
+function upperCase() {
+    var x = document.getElementById("textbox");
+    x.value = x.value.toUpperCase();
+    let text = x.value.split(".");
+    let addOn = text.join("-Moo");
+    x.value = addOn;
 }
